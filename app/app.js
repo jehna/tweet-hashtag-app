8fc3449e5ee34340d9c132283d47d61753f1ae53
jehna <jesse.luoto@gmail.com>
2015-09-17 21:34:46 +0300

    Fixed: iOS loading glitches

--- app/app.js
+++ app/app.js
@@ -3,12 +3,17 @@ angular.module('Hashtags', [
     // load your modules here
     'main', // starting with the main module
 ])
-.run(function ($window, $ionicPlatform, $location, Session) {
+.run(function ($window, $ionicPlatform, $location, Session, $timeout, $cordovaSplashscreen) {
     $ionicPlatform.ready(function () {
         $window.analytics.startTrackerWithId('UA-67235800-1');
+        $cordovaSplashscreen.hide();
     });
     
     if (!Session.isLoggedIn()) {
         $location.path('/login');
     }
+    
+    $timeout(function () {
+        $cordovaSplashscreen.hide();
+    }, 1000);
 });
